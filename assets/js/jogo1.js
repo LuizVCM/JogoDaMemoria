9561fff243b4480c94ab43882a8d3803281e9d90
LuizVCM <luizvcmluizvcm@gmail.com>
2026-04-28 17:01:25 -0300

    feat: adiciona parte inicial do frontend

---
+++ assets/js/jogo1.js
@@ -0,0 +1,6 @@
+const entendo = document.getElementById("entendi");
+const anuncio = document.getElementById("anuncio");
+
+entendo.addEventListener("click", ()=>{
+    anuncio.style.display = 'none';
+})
