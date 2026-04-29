99d18d08b3ec23e4a6f4a0c26b0341f81e6a3148
HENRY BITTENCOURT PADILHA <04498893000@senacrs.edu.br>
2026-04-29 15:27:34 -0300

    feature: alteração no home, adição das cartas nos modos.

--- assets/js/jogo1.js
+++ assets/js/jogo1.js
@@ -4,3 +4,16 @@ const anuncio = document.getElementById("anuncio");
 entendo.addEventListener("click", ()=>{
     anuncio.style.display = 'none';
 })
+
+const tabelaF1 = document.getElementById("card1")
+const tabelaF2 = document.getElementById("card2")
+const tabelaF3 = document.getElementById("card3")
+const tabelaF4 = document.getElementById("card4")
+const tabelaF5 = document.getElementById("card5")
+const tabelaF6 = document.getElementById("card6")
+const tabelaF7 = document.getElementById("card7")
+const tabelaF8 = document.getElementById("card8")
+const tabelaF9 = document.getElementById("card9")
+const tabelaF10 = document.getElementById("card10")
+const tabelaF11 = document.getElementById("card11")
+const tabelaF12 = document.getElementById("card12")
